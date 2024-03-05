9d6eb273b2f8e5b25fe6f08c096ee69f07705c03
Saint006 <saintunni1306@gmail.com>
2024-03-06 01:24:38 +0530

    front end and backend connection successful

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 
 const axios = require('axios');
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 
+
 const API_KEY = process.env.API_KEY;
 
+console.log('API_KEY:', API_KEY);
+app.use(cors());
 
 app.get('/',(req,res)=>{
-    const address = req.query.address;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${address}&units=metric&appid=${API_KEY}`
-
+    address = req.query.address;
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${address}&units=metric&appid=${API_KEY}`
+    console.log(url);
     axios.get(url).then(response=>{
+        console.log(url);
         const data = response.data;
         const cityName = data.name;
         const temperature = data.main.temp;
         const sunsetTime = new Date(data.sys.sunset * 1000).toLocaleTimeString();
-        const message = `City Name: ${cityName}<br>Temperature: ${temperature}&deg;C<br>Sunset Time: ${sunsetTime}`;
-        res.send(message);
+        const weatherData = {
+            cityName: cityName,
+            temperature: temperature,
+            sunsetTime: sunsetTime
+          };
+          res.json(weatherData);
 
     })
 .catch(error=>{
@@ -28,4 +38,4 @@ app.get('/',(req,res)=>{
 
 });
 
-app.listen(3000);
+app.listen(5500,()=>{console.log('connection established....')});
